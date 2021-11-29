01e72a8d0fe777a4763bf71c6e0e883bb132b11b
Falak <falak@hotmail.co.in>
2021-11-29 13:45:56 +0530

    3: Jumping On The Clouds - problem on hackerrank

--- jumpingOnTheClouds.js
+++ jumpingOnTheClouds.js
@@ -1,3 +1,64 @@
 `There is a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are cumulus. The player can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus  or . The player must avoid the thunderheads. Determine the minimum number of jumps it will take to jump from the starting postion to the last cloud. It is always possible to win the game.
 
 For each game, you will get an array of clouds numbered 0 if they are safe or 1 if they must be avoided.`
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+function jumpingOnClouds(arr) {
+    var count = 0;
+    // Write your code here
+    var i = 0;
+    while (i < arr.length - 1) {
+        if (i + 2 >= arr.length || arr[i + 2] == 1) {
+            i++
+            count++
+        } else {
+            i = i + 2
+            count++
+        }
+    }
+    return count
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const c = readLine().replace(/\s+$/g, '').split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const result = jumpingOnClouds(c);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
