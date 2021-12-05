2759ec23d0feb3130ca680194b88549695d9cbb9
Falak <falak@hotmail.co.in>
2021-12-05 19:46:29 +0530

    7: New Year Chaos - problem on hackerrank

---
+++ newYearChaos.js
@@ -0,0 +1,55 @@
+`It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from 1 to n. Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
+
+Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.`
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'minimumBribes' function below.
+ *
+ * The function accepts INTEGER_ARRAY q as parameter.
+ */
+
+function minimumBribes(q) {
+    // Write your code here
+    var count = 0;
+    for (var i = 0; i < q.length; i++) {
+        console.log(q[i], i + 1)
+        if (q[i] != i + 1) {
+            count++
+        }
+    }
+}
+
+function main() {
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const q = readLine().replace(/\s+$/g, '').split(' ').map(qTemp => parseInt(qTemp, 10));
+
+        minimumBribes(q);
+    }
+}
